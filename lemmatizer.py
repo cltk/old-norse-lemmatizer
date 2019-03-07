6088d84779e5b3db8025780cc8e04fcd935a58cc
clement_besnier <clemsciences@aol.com>
2019-03-07 18:09:54 +0100

    CLTK's backoff, eddas package and ZoegasLemmatizer are coming.

--- lemmatizer.py
+++ lemmatizer.py
@@ -3,6 +3,17 @@
 """
 from zoegas import reader
 
+from cltk.lemmatize import backoff
+from eddas import reader as eddas_reader
+
+voeluspaa = eddas_reader.PoeticEddaLemmatizationReader("Völuspá")
+
+voeluspaa.tagged_words()
+
+lemmatizer = backoff.IdentityLemmatizer()
+print(lemmatizer.lemmatize(voeluspaa.words()))
+
+
 dictionary = reader.Dictionary(reader.dictionary_name)
 
 entries = dictionary.get_entries()
@@ -21,3 +32,12 @@ segja = dictionary.find("mæla")
 print(segja.description)
 segja = dictionary.find("vaka")
 print(segja.description)
+
+
+class ZoegasLemmatizer:
+    """
+    The Zoëga's lemmatizer returns all the inflected forms from an entry.
+    """
+    def __init__(self):
+        pass
+
